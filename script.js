41931203c08485d661fabdab9f3458ac72d69af5
Rakesh R L <rlrakesh508@gmail.com>
2026-02-24 12:31:00 +0530

    Refactor math expression validation and calculation

--- script.js
+++ script.js
@@ -5,40 +5,39 @@ function solveDoubt(){
     // Remove spaces
     input = input.replace(/\s+/g, '');
 
-    // Check valid math expression
+    // Validate input
     if(!/^[0-9+\-*/.]+$/.test(input)){
-        response.innerHTML = "Please enter a valid math expression 😊";
+        response.innerHTML = "Please enter valid numbers with + - * / only 😊";
         return;
     }
 
-    try {
-        // Split numbers and operators
-        let numbers = input.split(/[\+\-\*\/]/).map(Number);
-        let operators = input.split(/[0-9\.]+/).filter(op => op);
+    // Split numbers and operators
+    let numbers = input.split(/[\+\-\*\/]/).map(Number);
+    let operators = input.match(/[\+\-\*\/]/g);
 
-        let result = numbers[0];
-
-        for(let i = 0; i < operators.length; i++){
-            if(operators[i] === '+'){
-                result += numbers[i+1];
-            }
-            else if(operators[i] === '-'){
-                result -= numbers[i+1];
-            }
-            else if(operators[i] === '*'){
-                result *= numbers[i+1];
-            }
-            else if(operators[i] === '/'){
-                result /= numbers[i+1];
-            }
-        }
-
-        response.innerHTML =
-        "Hi Champ! 😊<br>" +
-        input + " = <strong>" + result + "</strong><br>" +
-        "Math Made Easy 💡";
-
-    } catch {
-        response.innerHTML = "Something went wrong. Try again!";
+    if(!operators){
+        response.innerHTML = "Enter an expression like 10+20-5";
+        return;
     }
+
+    let result = numbers[0];
+
+    for(let i = 0; i < operators.length; i++){
+        if(operators[i] === '+'){
+            result = result + numbers[i+1];
+        }
+        else if(operators[i] === '-'){
+            result = result - numbers[i+1];
+        }
+        else if(operators[i] === '*'){
+            result = result * numbers[i+1];
+        }
+        else if(operators[i] === '/'){
+            result = result / numbers[i+1];
+        }
+    }
+
+    response.innerHTML =
+    "Hi Champ 😊<br>" +
+    input + " = <strong>" + result + "</strong>";
 }
